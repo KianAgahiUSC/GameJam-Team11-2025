5ea7071fea1f6ee189ccc77e09a72af3412efcf1
SaltedYellowe <smolraccoon325@gmail.com>
2025-11-08 02:19:28 -0800

    camera with mouse working

--- Assets/Scripts/Character/PlayerCamera.cs
+++ Assets/Scripts/Character/PlayerCamera.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace SG { 
+namespace SG {
     public class PlayerCamera : MonoBehaviour
     {
         public static PlayerCamera instance;
         public PlayerManager player;
         public Camera cameraObject;
+        [SerializeField] Transform cameraPivotTransform;
 
 
         [Header("Camera Settings")]
-        private Vector3 cameraVelocity;
         [SerializeField] private float cameraSmoothSpeed;
+
+        [SerializeField] private float upAndDownRotationSpeed = 220;
+        [SerializeField] private float leftAndRightRotationSpeed = 220;
+        [SerializeField] float minimumPivot = -30;
+        [SerializeField] float maximumPivot = 60;
+        [SerializeField] float cameraCollisionOffset = 0.2f;
+        [SerializeField] LayerMask collideWithLayers;
+
+        [Header("Camera Values")]
+        private Vector3 cameraVelocity;
+        private Vector3 cameraObjectPosition;
+        [SerializeField] float leftAndRightLookAngle;
+        [SerializeField] float upAndDownLookAngle;
+
+        private float cameraZPosition;
+        private float targetCameraZPosition;
         private void Awake()
         {
             cameraSmoothSpeed = 1;
@@ -30,6 +46,7 @@ namespace SG {
         private void Start()
         {
             DontDestroyOnLoad(gameObject);
+            cameraZPosition = cameraObject.transform.localPosition.z;
         }
 
         public void HandleAllCameraActions()
@@ -38,6 +55,7 @@ namespace SG {
             {
                 Debug.Log("FOLLOW");
                 FollowTarget();
+                HandleRotations();
             }
         }
 
@@ -45,6 +63,53 @@ namespace SG {
         {
             Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, player.transform.position, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
             transform.position = targetCameraPosition;
+        }
+
+        private void HandleRotations()
+        {
+
+
+
+            leftAndRightLookAngle += (PlayerInputManager.instance.cameraHorizontalInput * leftAndRightRotationSpeed) * Time.deltaTime;
+            upAndDownLookAngle -= (PlayerInputManager.instance.cameraVerticalInput * upAndDownRotationSpeed) * Time.deltaTime;
+            upAndDownLookAngle = Mathf.Clamp(upAndDownLookAngle, minimumPivot, maximumPivot);
+
+            Vector3 cameraRotation = Vector3.zero;
+            Quaternion targetRotation;
+
+            cameraRotation.y = leftAndRightLookAngle;
+            targetRotation = Quaternion.Euler(cameraRotation);
+            transform.rotation = targetRotation;
+
+            cameraRotation = Vector3.zero;
+            cameraRotation.x = upAndDownLookAngle;
+            targetRotation = Quaternion.Euler(cameraRotation);
+            cameraPivotTransform.localRotation = targetRotation;
+
+
+        
+        }
+
+        private void HandleCollisions()
+        {
+            targetCameraZPosition = cameraZPosition;
+            RaycastHit hit;
+            Vector3 direction = cameraObject.transform.position - cameraPivotTransform.position;
+            direction.Normalize();
+
+            if (Physics.SphereCast(cameraPivotTransform.position, cameraCollisionOffset, direction, out hit, Mathf.Abs(targetCameraZPosition), collideWithLayers))
+            {
+                float distanceFromHitObject = Vector3.Distance(cameraPivotTransform.position, hit.point);
+                targetCameraZPosition = -(distanceFromHitObject - cameraCollisionOffset);
+            }
+
+            if (Mathf.Abs(targetCameraZPosition) < cameraCollisionOffset)
+            {
+                targetCameraZPosition = -cameraCollisionOffset;
+            }
+
+            cameraObjectPosition.z = Mathf.Lerp(cameraObject.transform.localPosition.z, targetCameraZPosition, 0.2f);
+            cameraObject.transform.localPosition = cameraObjectPosition;
         }
     }
 }
--- Assets/Scripts/Character/PlayerInputManager.cs
+++ Assets/Scripts/Character/PlayerInputManager.cs
@@ -6,74 +6,93 @@ using System;
 using UnityEngine.Serialization;
 using UnityEngine.SceneManagement;
 
-[System.Serializable]
-public class PlayerInputManager : MonoBehaviour
+
+namespace SG
 {
-    UserActions playerControls;
-
-    public static PlayerInputManager instance;
-
-    [SerializeField] Vector2 movementInput;
-    public float verticalInput;
-    public float horizontalInput;
-    public float moveAmount;
-
-    private void Awake()
+    public class PlayerInputManager : MonoBehaviour
     {
-        if (instance == null)
+        UserActions playerControls;
+
+        public static PlayerInputManager instance;
+
+        [Header("PLAYER MOVEMENT INPUT")]
+        [SerializeField] Vector2 movementInput;
+        public float verticalInput;
+        public float horizontalInput;
+        public float moveAmount;
+
+        [Header("CAMERA MOVEMENT INPUT")]
+        [SerializeField] Vector2 cameraInput;
+        public float cameraVerticalInput;
+        public float cameraHorizontalInput;
+
+        private void Awake()
         {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-        SceneManager.activeSceneChanged += OnSceneChange;
-    }
-
-    private void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-
-        instance.enabled = true;
-
-        
-    }
-
-    private void OnSceneChange(Scene oldScene, Scene newScene) 
-    {
-        instance.enabled = true;
-    }
-
-    private void OnEnable()
-    {
-        if (playerControls == null)
-        {
-            playerControls = new UserActions();
-             
-            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
-            playerControls.PlayerMovement.Movement.canceled += i => movementInput = i.ReadValue<Vector2>();
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            SceneManager.activeSceneChanged += OnSceneChange;
         }
 
-        playerControls.Enable();
-    }
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
 
-    private void OnDestroy()
-    {
-        SceneManager.activeSceneChanged -= OnSceneChange;
-    }
+            instance.enabled = true;
 
-    private void HandleMovementInput()
-    {
-        verticalInput = movementInput.y;
-        horizontalInput = movementInput.x;
-        //Debug.Log(verticalInput);
-        //Debug.Log(horizontalInput);
-        moveAmount = Mathf.Clamp01(Mathf.Abs(verticalInput) + Mathf.Abs(horizontalInput));
-    }
 
-    private void Update()
-    {
-        HandleMovementInput();
+        }
+
+        private void OnSceneChange(Scene oldScene, Scene newScene)
+        {
+            instance.enabled = true;
+        }
+
+        private void OnEnable()
+        {
+            if (playerControls == null)
+            {
+                playerControls = new UserActions();
+
+                playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+                playerControls.PlayerMovement.Movement.canceled += i => movementInput = i.ReadValue<Vector2>();
+
+                playerControls.PlayerCamera.Movement.performed += i => cameraInput = i.ReadValue<Vector2>();
+                playerControls.PlayerCamera.Movement.canceled += i => cameraInput = i.ReadValue<Vector2>();
+            }
+
+            playerControls.Enable();
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= OnSceneChange;
+        }
+
+        private void HandlePlayerMovementInput()
+        {
+            verticalInput = movementInput.y;
+            horizontalInput = movementInput.x;
+            //Debug.Log(verticalInput);
+            //Debug.Log(horizontalInput);
+            moveAmount = Mathf.Clamp01(Mathf.Abs(verticalInput) + Mathf.Abs(horizontalInput));
+        }
+
+        private void HandleCameraMovementInput()
+        {
+            cameraVerticalInput = cameraInput.y;
+            cameraHorizontalInput = cameraInput.x;
+        }
+
+        private void Update()
+        {
+            HandlePlayerMovementInput();
+            HandleCameraMovementInput();
+        }
     }
 }
